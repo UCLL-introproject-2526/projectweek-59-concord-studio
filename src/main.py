6cd726c9efab94dcc3d4ab9e5f871319cc1f5085
Vedat Cingoz <vedat.cingoz@student.kuleuven.be>
2025-12-15 14:32:42 +0100

    added main.py

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import pygame
+import player
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Simple Pygame Window")
+    clock = pygame.time.Clock()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((255, 255, 255))
+
+
+
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
