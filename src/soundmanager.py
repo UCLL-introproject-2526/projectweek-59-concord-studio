ee514b4b73a0e1d25fefc586e12e17825c2622a4
Vedat Cingoz <vedat.cingoz@student.kuleuven.be>
2025-12-17 10:37:25 +0100

    soundmanager

---
+++ src/soundmanager.py
@@ -0,0 +1,19 @@
+import pygame
+
+class SoundManager:
+    def __init__(self):
+        pygame.mixer.init()
+        self.sounds = {
+            "chase": pygame.mixer.Sound("../sfx/audio cop chase.wav"),
+            "bike_throw": pygame.mixer.Sound("../sfx/bike throw.wav"),
+            "background": pygame.mixer.Sound("../sfx/ingame ambient music.wav")
+        }
+
+    def play_sound(self, sound_name, volume=1, loops=0):
+        if sound_name in self.sounds:
+            self.sounds[sound_name].set_volume(volume)
+            self.sounds[sound_name].play(loops)
+
+    def stop_sound(self, sound_name):
+        if sound_name in self.sounds:
+            self.sounds[sound_name].stop()
